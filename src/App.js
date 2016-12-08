505de635dd46d3c5828f7e64fce00c86f93fb32e
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-07 20:06:05 -0800

    Loads infinite grid of photos from json

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
+import InfiniteGrid from 'react-infinite-grid';
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import findLast from 'lodash.findlast';
 import './App.css';
+import Photo from './Photo.js';
+import InstagramData from '../tmp/results.json';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: [],
+      gridHeight: props.gridHeight
+    };
+  }
+  componentDidMount(x,y,z){
+    this.setState({
+      gridHeight:`${window.innerHeight}px`
+    });
+  }
+  componentWillMount () {
+    InstagramData.map((photoData, index) => {
+      const key = `photo-${index}`;
+      const photoSrc = findLast(photoData.images, image => (image.width >= 300));
+      return this.state.photos.push(<Photo src={photoSrc.url} title={photoData.location.title} index={index} key={key} />);
+    });
+  }
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+    return (<div className='App'>
+        <div className='App-header'>
+          <h2>Café Fronts</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+        <InfiniteGrid
+          padding={0}
+          buffer={0}
+          wrapperHeight={this.state.gridHeight}
+          entries={this.state.photos}
+        />
+        </div>);
   }
 }
 
---
+++ src/Photo.js
@@ -0,0 +1,4 @@
+import React from 'react';
+const Photo = (props) => (<div className="instagram-photo"><img src={props.src} alt={props.title} /></div>);
+
+export default Photo;
