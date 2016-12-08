891dd987fd979946a605219470235d2216bac4e3
Pamela Ocampo <pamela.ocampo@gmail.com>
2016-12-08 14:05:55 -0800

    Adds styling around photos and more details about each one

--- src/Photo.js
+++ src/Photo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 const toSecureUrl = (url) => (url.replace(/^http/, 'https'));
 const Photo = (props) => (<div className="instagram-photo">
+    <h3>{props.title}</h3>
+    <h4>{props.location.city}</h4>
     <a href={props.link} target="_blank" rel="noreferrer noopener"><img src={toSecureUrl(props.src)} alt={props.title} /></a>
     </div>);
 
